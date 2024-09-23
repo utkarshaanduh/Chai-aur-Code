ed9fcf95bac4270692f4c2c4ea5ac5c7389dfeaf
Utkarsh <99259619+utkarshaanduh@users.noreply.github.com>
2024-09-23 19:45:41 +0000

    commit for variables

---
+++ basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // by this we will be treating
+// the code as the newer version of the javascript
+
+// alert("hello") we are using node js not browser therefore errors
+console.log("a"+3)
+
+/*
+    There are various types of datatypes in js
+        int 
+        bigint
+        string
+        boolean
+        null is a stand alone value (Empty value)
+        undefined value isn't assigned
+        symbol for uniqueness
+
+        object we will be talking alot in terms of objects
+*/
+
+console.log(typeof null)
+// It will output as object therefore null is an object
+// Type of undefined is undefined in js
---
+++ basics/variables.js
@@ -0,0 +1,10 @@
+const account_id = 144553
+let account_email = "hitesh@gmail.com"
+var account_password = "12345"
+let accountCity;
+
+// account_id = 2
+account_email = "hc@hc.com"
+account_password = "212121221212122112111112"
+
+console.table([account_email, account_id, account_password, accountCity])
