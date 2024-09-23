5dec1910f847c7c85617a9f276f54aa17310ace2
Utkarsh <99259619+utkarshaanduh@users.noreply.github.com>
2024-09-23 20:46:05 +0000

    commit comparisons

---
+++ basics/comparisons.js
@@ -0,0 +1,14 @@
+// console.log(null > 0);  //ans -> false
+// console.log(null == 0); //ans -> false
+// console.log(null >= 0); // ans -> true here null gets converted to Zero 0
+//                         therefore the answer is true
+
+/*
+    This is due to different behaviour of comparison and 
+    equality operator
+*/
+
+//console.log(undefined >= 0); // any operator it will be 
+                            // "Permission Denied"
+
+// Strict check: checks for only same datatype...otherwise "false"
