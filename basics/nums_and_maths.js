61f81f16c83d5f0613e5cb08e338e614236d10b3
Utkarsh <99259619+utkarshaanduh@users.noreply.github.com>
2024-09-24 16:16:13 +0000

    maths and num commit

---
+++ basics/nums_and_maths.js
@@ -0,0 +1,29 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2)); // toFixed decimal places, here 2
+
+const num = 1123.35480353
+console.log(num.toPrecision(5));
+
+// It will return exponential value for precision less than the whole number
+// and roundoff value for precision greater than whole number
+
+const hundreds = 100000000
+console.log(hundreds.toLocaleString());
+// .toLocaleString for commas in string
+// .toLocaleString('en-IN') for commas in Indian style of writing
+
+
+// ---------------------------Maths-----------------------------
+
+const max = 6;
+const min = 1;
+
+console.log(Math);
+console.log(Math.floor(Math.random()*(max - min +1)) + min);
+
--- basics/string.js
+++ basics/string.js
@@ -4,18 +4,26 @@
 */
 
 const name = "Hitesh"
-const repocount = 50
-console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+// const repocount = 50
+// console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
 
-// we may declare a string using the method below:
+// // we may declare a string using the method below:
 
-const new_name = new String("Hitesh");
-console.log(`Hello my name is ${new_name} and my repo count is ${repocount}`)
+// const new_name = new String("Hitesh");
+// console.log(`Hello my name is ${new_name} and my repo count is ${repocount}`)
 
-console.log(new_name[1]);
-console.log(new_name.toLocaleUpperCase());
-console.log(new_name.toUpperCase());
-console.log(new_name);
+// console.log(new_name[1]);
+// console.log(new_name.toLocaleUpperCase());
+// console.log(new_name.toUpperCase());
+// console.log(new_name);
 
+// const variable = new_name.substring(0,4)
+// console.log(variable);
 
+// const another_string = new_name.slice(-8,2)
+// console.log(another_string);
 
+// ".trim()" is used to remove starting and ending spaces of the string
+// ".replace(a,b)" is used to remove "a" and replace it to "b"
+// ".includes(a)" is used to ask if "a" is in the string
+// ".split("/")" it will split the stiring on every "/"
