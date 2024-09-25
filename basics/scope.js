75f1cf5a46a9e74e7cabb44ca86ff2c8e85ed4bc
Utkarsh <99259619+utkarshaanduh@users.noreply.github.com>
2024-09-25 11:20:33 +0000

    commit kar lo

---
+++ basics/scope.js
@@ -0,0 +1,21 @@
+/*
+    scope ki kahaani shuru hoti hai let, var aur const se
+*/
+
+/*
+    curly braces are called scope in terms of functions 
+    and conditional statements
+
+    The problem with var is that it dosen't follow the 
+    scope it still acts as a global variable
+
+    scopes in terms of coding environment is different 
+    from the scope for windows
+*/
+
+// Nested scope and  
+
+
+
+
+
