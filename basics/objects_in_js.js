e6e3aa55955f95de43024aff2c63052beba7bef9
Utkarsh <99259619+utkarshaanduh@users.noreply.github.com>
2024-09-25 06:53:49 +0000

    objects in js

---
+++ basics/objects_in_js.js
@@ -0,0 +1,68 @@
+// singleton: "object.create"
+
+// object literals
+
+// const mysym = Symbol("mykey1") // first define a symbol
+
+// const Jsuser = {
+//     name: "Hitesh",
+//     "fullname": "hitesh chaudhary",
+//     [mysym]: "1", // key value pair for symbol
+//     age: 18,
+//     email: "hello@google.com",
+//     isloggedin: true,
+//     lastloggedin: ["Monday", "saturday"]
+// }
+
+// console.log(Jsuser.isloggedin)
+
+// console.log(Jsuser["email"]);
+
+// console.log(Jsuser);
+
+// Freezing the object (no changes will occur in the object)
+//Object.freeze(Jsuser);
+
+// Jsuser.greeting = function(){
+//     console.log("Hello jsuser");
+// }
+
+// Jsuser.greeting2 = function(){
+//     console.log(`Hello Js User, ${this.name}`);
+// }
+
+// console.log(Jsuser.greeting());
+// console.log(Jsuser.greeting2());
+
+
+/*
+    Now we will be talking about the singleton objects 
+    in javascript
+*/ 
+
+// cosnt tinderUser = new Object()
+
+const tinderUser ={}
+
+tinderUser.id = "1233hfsdo"
+tinderUser.name = "Sam"
+//console.log(tinderUser);
+
+const regularUser = {
+    email: "something@gmail.com",
+    names: "abc"
+}
+
+// console.log(regularUser.fullname.username.firstName);
+
+
+// // Merging two or more objects...
+// const ans = Object.assign({}, tinderUser, regularUser)
+// console.log(ans);
+// // OR we will use spread syntax
+// const obj3 = {...tinderUser, ...regularUser}
+// console.log(obj3);
+
+
+
+
