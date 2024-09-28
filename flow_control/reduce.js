124f9fe70f53c3f9738f83fcf5f286c5521538f6
Utkarsh <99259619+utkarshaanduh@users.noreply.github.com>
2024-09-28 07:51:00 +0000

    reduce is comitted

--- flow_control/reduce.js
+++ flow_control/reduce.js
@@ -13,3 +13,29 @@ const nums = [1, 2, 3]
 const mytotal = nums.reduce((acc, currval)=>(acc+currval),0)
 
 console.log(mytotal);
+
+const course = [
+    {
+        itemname: "js course",
+        price: 999
+    },
+    {
+        itemname: "pycouse",
+        price: 1999
+    },
+    {
+        itemname: "mobile dev",
+        price: 3999
+    },
+    {
+        itemname: "Data science",
+        price: 2999
+    },
+]
+
+// Here we will add the price of selected courses:
+
+const shoppingCart = course.reduce((acc, item) => (acc + item.price)
+, 0)
+
+console.log(shoppingCart);
